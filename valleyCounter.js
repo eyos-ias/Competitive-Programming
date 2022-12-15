f61eebed5ec2f1aa1a4be6614b0bbf52ef94da11
Eyosias Tamirat <75771087+eyos-ias@users.noreply.github.com>
2022-12-15 21:52:44 +0300

    HackerRank

---
+++ valleyCounter.js
@@ -0,0 +1,28 @@
+function countingValleys(steps, path) {
+    let valleys = 0;
+    let height = 0;
+    let isSeaLevel = true;
+    for (const dir of path) {
+        if (isSeaLevel) {
+            isSeaLevel = false;
+            if (dir === 'U') {
+                height += 1;
+            }
+            if (dir === 'D') {
+                height -= 1;
+                valleys += 1;
+            }
+            continue;
+        }
+        if (dir === 'U') {
+            height += 1;
+        }
+        if (dir === 'D') {
+            height -= 1;
+        }
+        if (height === 0) {
+            isSeaLevel = true;
+        }
+    }
+    return valleys;
+}
