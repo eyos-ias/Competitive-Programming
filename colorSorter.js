85b3553af8411736b1a3013ad4ae5b45340bcbc7
Eyosias Tamirat <75771087+eyos-ias@users.noreply.github.com>
2022-12-15 21:41:41 +0300

    LeetCode

---
+++ colorSorter.js
@@ -0,0 +1,10 @@
+function sortColors(nums) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] >= nums[j]) {
+        [nums[i], nums[j]] = [nums[j], nums[i]];
+      }
+    }
+  }
+  return nums;
+}
